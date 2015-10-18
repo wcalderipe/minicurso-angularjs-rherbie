68dce884e20ef7a7704a4237a1c3c7263fa55452
William Calderipe <wcalderipe@gmail.com>
2015-10-18 19:39:59 -0200

    Interando um array estático de carros usando o ng-repeat

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,9 +5,55 @@
     .module('app')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = [];
+  HomeController.$inject = ['$scope'];
 
-  function HomeController() {
+  function HomeController($scope) {
+    $scope.cars = [
+      {
+        "id": 1,
+        "model": "Lorem ipsum dolor",
+        "brand": "Ford",
+        "year": 2015,
+        "currency": "R$",
+        "value": 34500,
+        "seller": "Revenda Herbie",
+        "cover_image": "images/demo1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
+      },
+      {
+        "id": 2,
+        "model": "Lorem ipsum dolor",
+        "brand": "Ford",
+        "year": 2015,
+        "currency": "R$",
+        "value": 34500,
+        "seller": "Revenda Herbie",
+        "cover_image": "images/demo1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
+      },
+      {
+        "id": 3,
+        "model": "Lorem ipsum dolor",
+        "brand": "Ford",
+        "year": 2015,
+        "currency": "R$",
+        "value": 34500,
+        "seller": "Revenda Herbie",
+        "cover_image": "images/demo1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
+      },
+      {
+        "id": 4,
+        "model": "Lorem ipsum dolor",
+        "brand": "Ford",
+        "year": 2015,
+        "currency": "R$",
+        "value": 34500,
+        "seller": "Revenda Herbie",
+        "cover_image": "images/demo1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
+      }
+    ];
   }
 })();
 
