70594b3a8b770a6f849fc9965214768ded6d0b80
William Lima <wlima@conrad-caine.com>
2015-10-19 12:38:04 -0200

    Adicionado controle e template de contatos

--- app/app.js
+++ app/app.js
@@ -10,6 +10,10 @@
           templateUrl: 'app/home/home.tmpl.html',
           controller: 'HomeController'
         })
+        .when('/contact', {
+          templateUrl: 'app/contact/contact.tmpl.html',
+          controller: 'ContactController'
+        })
         .otherwise({
           redirectTo: '/home'
         });
---
+++ app/contact/contact.controller.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('ContactController', ContactController);
+
+  ContactController.$inject = ['$scope'];
+
+  function ContactController($scope) {
+
+  }
+})();
+
