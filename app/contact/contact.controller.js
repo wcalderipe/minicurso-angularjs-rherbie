fd1aa06ab84070ad310d1722ac845c718595d36c
William Lima <wlima@conrad-caine.com>
2015-10-19 13:38:22 -0200

    Adicionada validação no formulário

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -8,7 +8,21 @@
   ContactController.$inject = ['$scope'];
 
   function ContactController($scope) {
+    $scope.message = {
+      name: '',
+      email: '',
+      body: ''
+    };
 
+    $scope.email = '';
+
+    $scope.submit = function() {
+      if (!$scope.contactForm.$valid) {
+        return alert('invalid');
+      }
+
+      alert('valid\n' + JSON.stringify($scope.message));
+    };
   }
 })();
 
