b1f454636338b8a33e873dff345deaa3af26c251
William Calderipe <wcalderipe@gmail.com>
2015-10-20 01:55:19 -0200

    Removido o array de carros do home controller e adicionado o CarsFactory

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,57 +5,12 @@
     .module('app')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope', '$filter'];
+  HomeController.$inject = ['$scope', '$filter', 'CarsFactory'];
 
-  function HomeController($scope, $filter) {
+  function HomeController($scope, $filter, CarsFactory) {
     var orderBy = $filter('orderBy');
 
-    $scope.cars = [
-      {
-        "id": 1,
-        "model": "Lorem ipsum dolor",
-        "brand": "Ford",
-        "year": 2015,
-        "currency": "R$",
-        "value": 40000,
-        "seller": "Revenda Herbie",
-        "cover_image": "images/demo1.jpg",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
-      },
-      {
-        "id": 2,
-        "model": "Lorem ipsum dolor",
-        "brand": "Volkswagen",
-        "year": 2015,
-        "currency": "R$",
-        "value": 20500,
-        "seller": "Revenda Herbie",
-        "cover_image": "images/demo1.jpg",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
-      },
-      {
-        "id": 3,
-        "model": "Lorem ipsum dolor",
-        "brand": "Honda",
-        "year": 2015,
-        "currency": "R$",
-        "value": 30000,
-        "seller": "Revenda Herbie",
-        "cover_image": "images/demo1.jpg",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
-      },
-      {
-        "id": 4,
-        "model": "Lorem ipsum dolor",
-        "brand": "Chevrolet",
-        "year": 2015,
-        "currency": "R$",
-        "value": 30500,
-        "seller": "Revenda Herbie",
-        "cover_image": "images/demo1.jpg",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nostrum adipisci ipsam dolore ullam aperiam autem nesciunt soluta. Rerum illo facilis accusantium quis ducimus earum cumque provident porro nihil quo.",
-      }
-    ];
+    $scope.cars = CarsFactory.getCars();
 
     $scope.sortPredicate = '';
 
